d6497fb176031ac1034ba00fcbe631ba06a43146
Michał Kotfica <kotvitz17@gmail.com>
2024-02-12 13:55:55 +0100

    add header checking

--- src/main/java/com/example/githubrepo/controller/GithubRepositoryController.java
+++ src/main/java/com/example/githubrepo/controller/GithubRepositoryController.java
@@ -2,9 +2,12 @@ package com.example.githubrepo.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.UnsupportedMediaTypeException;
 
 import com.example.githubrepo.model.Repository;
 import com.example.githubrepo.service.GithubRepositoryService;
@@ -20,8 +23,12 @@ public class GithubRepositoryController {
 		this.githubRepositoryService = githubRepositoryService;
 	}
 
-	@GetMapping("/repositories/{username}")
-	public Mono<List<Repository>> getUserRepositories(@PathVariable String username) {
+	@GetMapping(value = "/repositories/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Mono<List<Repository>> getUserRepositories(@PathVariable String username,
+			@RequestHeader(value = "Accept", defaultValue = MediaType.APPLICATION_JSON_VALUE) String acceptHeader) {
+		if (!MediaType.APPLICATION_JSON_VALUE.equals(acceptHeader)) {
+			return Mono.error(new UnsupportedMediaTypeException("Only 'application/json' content type is supported."));
+		}
 		return githubRepositoryService.getUserRepositories(username);
 	}
 }
